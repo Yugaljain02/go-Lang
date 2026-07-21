cafd6c4e723a8b06ec75e883b79575d8e253cc6e
Yugal Jain <jyugaljain@gmail.com>
2026-07-21 17:27:04 +0530

    changes in visibility

---
+++ Visibility/calculator/calculator.go
@@ -0,0 +1,8 @@
+package calculator
+
+func Add(a,b int) int {
+	return a+b
+}
+func Sub(a,b int) int {
+	return a-b
+}
---
+++ Visibility/private_public.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"GoProject/Visibility/calculator"
+	"fmt"
+	 "github.com/gin-gonic/gin"
+)
+
+func main() {
+	a := 12
+	b := 20
+	fmt.Println(calculator.Add(a, b))
+	fmt.Println(calculator.Sub(a, b))
+}
